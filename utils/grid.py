05f051f471ce961b2d1f3dcb5d5c8cf1131abfbb
Anders Åstrand <anders@teknodromen.se>
2022-12-09 14:36:40 +0100

    Implement nicer grid class

--- utils/grid.py
+++ utils/grid.py
@@ -10,24 +10,39 @@ TOP_RIGHT = (1, -1)
 BOTTOM_LEFT = (-1, 1)
 
 
-OFFSETS = (RIGHT, LEFT, BOTTOM, TOP, TOP_LEFT, BOTTOM_RIGHT, TOP_RIGHT, BOTTOM_LEFT)
+OFFSETS = (RIGHT, LEFT, BOTTOM, TOP, TOP_LEFT,
+           BOTTOM_RIGHT, TOP_RIGHT, BOTTOM_LEFT)
 OFFSETS_STRAIGHT = (RIGHT, LEFT, TOP, BOTTOM)
 
 
-Grid = namedtuple("Grid", "data minX maxX minY maxY")
+class Grid:
+    def __init__(self):
+        self.data = {}
+        self.minX = 0
+        self.maxX = 0
+        self.minY = 0
+        self.maxY = 0
 
+    def set(self, coords, val):
+        self.set_at(coords[0], coords[1], val)
 
-def print_grid(grid):
-    _print_grid(grid.data, grid.maxX, grid.maxY)
+    def set_at(self, x, y, val):
+        self.minX = min(self.minX, x)
+        self.maxX = max(self.maxX, x)
+        self.minY = min(self.minY, y)
+        self.maxY = max(self.maxY, y)
+        self.data[(x, y)] = val
 
+    def get(self, x, y):
+        return self.data.get(x, y)
 
-def _print_grid(grid, maxX=128, maxY=128, default="."):
-    for y in range(maxY):
-        r = []
-        for x in range(maxX):
-            r.append(str(grid.get((x, y), default)))
-        print("".join(r))
-    print()
+    def print(self, default="."):
+        for y in range(self.minY, self.maxY+1):
+            r = []
+            for x in range(self.minX, self.maxX+1):
+                r.append(str(self.data.get((x, y), default)))
+            print("".join(r))
+        print()
 
 
 def flood_fill(grid, pos, visitor):
