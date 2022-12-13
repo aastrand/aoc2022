beb7a5427230e911b7c83d97445f5f19a66ac2f9
Anders Åstrand <anders@teknodromen.se>
2022-12-13 20:52:04 +0100

    run.sh in order

--- run.sh
+++ run.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-for d in day*; do
-  cd $d; 
-  if [[ -e "./$d.py" ]]; then
-    echo "Running ${d}"
-    PYTHONPATH=..:. ./$d.py;
+for d in {1..25}; do
+  if [[ -e "day${d}/day${d}.py" ]]; then
+    cd day$d;
+    echo "Running day${d}"
+
+    PYTHONPATH=..:. ./day$d.py;
     r=$?
     if [ $r -ne 0 ]; then
       echo "Error running ${d}"
       exit $r
     fi
+
     echo ""
+    cd $OLDPWD;
   fi
-  cd $OLDPWD; 
+
 done
